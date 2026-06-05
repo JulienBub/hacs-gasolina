fe5bd0170103c47a879b5a65f3fe3475463f38fe
JulienBub <36966472+JulienBub@users.noreply.github.com>
2026-06-05 14:12:27 +0200

    refactor: remove temperature sensor (not in device data)

--- custom_components/gasolina/const.py
+++ custom_components/gasolina/const.py
@@ -8,10 +8,10 @@ LOCAL_NAME_PREFIX = "@UTS"
 # Byte offsets within manufacturer data (after company ID bytes are stripped by HA)
 # Confirmed via BLE advertisement sniffing (nRF Connect iOS):
 #   data[1]  = raw ultrasound distance (sensor-internal unit; varies with bottle size)
-#   data[2]  = temperature in °C
+#   data[2]  = unknown (constant, possibly firmware version or calibration)
+#   data[3]  = unknown (constant)
 #   data[4]  = fill level in % (calculated by device firmware using its stored bottle size)
 #   data[6]  = battery level in %
-OFFSET_TEMPERATURE = 2
 OFFSET_FILL_LEVEL = 4
 OFFSET_BATTERY = 6
 
